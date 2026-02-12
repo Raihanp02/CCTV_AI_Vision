242bf4ab485f76359cf040b52c1463f4c17bb431
raihan.pahlevi <raihan.pahlevi@aqi.co.id>
2026-02-12 13:41:10 +0700

    fix: prediction batch across cctv

--- app/pipelines/face_pipeline/facial_expression_pipeline.py
+++ app/pipelines/face_pipeline/facial_expression_pipeline.py
@@ -12,13 +12,14 @@ class FacialExpressionPipeline(BasePipeline):
         self.detection_interval = detection_interval
     
     def process(self, face_info):
+        face_list = []
+        id_list = []
+        index_list = []
+        cam_id_list = []
+
         for cam_id, value in face_info.items():
             detections = value.get("detections")
             facial_info = detections.get("facial_info")
-
-            face_list = []
-            id_list = []
-            index_list = []
 
             for i, info in enumerate(facial_info):
                 face = info.get("face_crop")
@@ -32,18 +33,24 @@ class FacialExpressionPipeline(BasePipeline):
                     face_list.append(face)
                     id_list.append(id)
                     index_list.append(i)
+                    cam_id_list.append(cam_id)
             
-            if face_list:
-                prediction = self.module.detect(face_list)
+        if face_list:
+            prediction = self.module.detect(face_list)
 
-                for index, predict in enumerate(prediction):
-                    id = id_list[index]
-                    face_info = facial_info[index_list[index]]
-                    track_status = face_info.get("tracked_status")
-                    if predict:
-                        if not track_status:
-                            self.tracked_data[cam_id].init_track_info(id)
-                        self.tracked_data[cam_id].update_prediction_info(id, predict, FacialExpressionPipeline.name)
+            for index, predict in enumerate(prediction):
+                cam_id = cam_id_list[index]
+                detections = face_info[cam_id].get("detections")
+                facial_info = detections.get("facial_info")
+
+                id = id_list[index]
+                face_info = facial_info[index_list[index]]
+                track_status = face_info.get("tracked_status")
+    
+                if predict:
+                    if not track_status:
+                        self.tracked_data[cam_id].init_track_info(id)
+                    self.tracked_data[cam_id].update_prediction_info(id, predict, FacialExpressionPipeline.name)
 
     
             
